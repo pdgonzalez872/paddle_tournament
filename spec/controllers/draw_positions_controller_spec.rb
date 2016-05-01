304685fb4367b74723cebe064929655cfc419b91
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-01 16:30:12 -0500

    Try controller spec first, will do integration spec first.

--- spec/controllers/draw_positions_controller_spec.rb
+++ spec/controllers/draw_positions_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe DrawPositionsController, type: :controller do
+
+  xdescribe '#edit' do
+    it 'is successful' do
+      get edit_tournament_draw_draw_position_path(id: 1,
+                                                            draw_id: 1,
+                                                            tournament_id: 1)
+
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'update' do
+
+  end
 end
