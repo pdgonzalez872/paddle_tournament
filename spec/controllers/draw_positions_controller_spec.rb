eb65e92254353580f202fa7c54dfb02a3c3318f6
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-08 17:32:29 -0500

    Get request now works

--- spec/controllers/draw_positions_controller_spec.rb
+++ spec/controllers/draw_positions_controller_spec.rb
@@ -3,10 +3,23 @@ require 'rails_helper'
 RSpec.describe DrawPositionsController, type: :controller do
 
   describe '#edit' do
+    let(:path) { edit_tournament_draw_draw_position_path(tournament_id: tournament.id, draw_id: draw.id, id: draw_position.id) }
     let(:tournament) { FactoryGirl.create(:tournament, name: 'wimbledon') }
+    let(:draw) { FactoryGirl.create(:draw, tournament: tournament) }
+    let(:draw_position) { FactoryGirl.create(:draw_position, draw: draw) }
+    let(:player) { FactoryGirl.create(:player, draw_position: draw_position) }
 
     it 'makes a tournament' do
       expect(tournament.name).to eq('wimbledon')
+      expect(draw_position.draw.tournament.name).to eq('wimbledon')
+    end
+
+    it "renders the previous match's players" do
+      get :edit, id: draw_position.id, tournament_id: tournament.id, draw_id: draw.id
+
+      #    nokogiri
+      # parse response, find css
+
     end
   end
 
