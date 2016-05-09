9e0f156f06c26c1b2221ecc12fe6ea2d363977a8
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-09 08:04:12 -0500

    Add spec that shows winners can be added to matches

--- spec/features/advance_players_spec.rb
+++ spec/features/advance_players_spec.rb
@@ -11,15 +11,19 @@ describe 'when I visit the edit draw position page after seeding the test databa
   end
 
   context 'has the main feature available' do
-    it 'can advance players correctly' do
 
+    let(:player_1) { Player.find_by(name: 'Laird/Watkins') }
+    let(:draw)     { Draw.find(1) }
+    let(:match)    { draw.matches.find_by(match_number: 16) }
+
+    it 'can advance players correctly and adds winners to matches' do
       advance_winner(draw_position_id: 16, winner_name: 'Laird/Watkins')
-
       advance_winner(draw_position_id: 17, winner_name: 'Brace/Slepian')
 
       visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: 8)
 
       expect(page).to have_text('Laird/Watkins')
+      expect(player_1.id).to eq(match.winner_id)
     end
   end
 
