d6c9c60a81065710688d12eb6a7c9b737f002649
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-09 07:02:17 -0500

    Refactor test

--- spec/features/draw_position_spec.rb
+++ spec/features/draw_position_spec.rb
@@ -10,27 +10,26 @@ describe 'when I visit the edit draw position page after seeding the test databa
     expect(page).to have_text("Laird/Watkins")
   end
 
-  it 'can advance players correctly' do
+  context 'has the main feature available' do
+    it 'can advance players correctly' do
 
-    visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: 16)
+      advance_winner(draw_position_id: 16, winner_name: 'Laird/Watkins')
 
-    select('Laird/Watkins', from: "draw_position_winner")
+      advance_winner(draw_position_id: 17, winner_name: 'Brace/Slepian')
 
+      visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: 8)
+
+      expect(page).to have_text('Laird/Watkins')
+    end
+  end
+
+  def advance_winner(draw_position_id:, winner_name: )
+    visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: draw_position_id)
+
+    select(winner_name, from: "draw_position_winner")
     fill_in "draw_position_score", with: 'Test Score'
 
     click_button("Update Draw position")
-
-    visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: 17)
-
-    select('Brace/Slepian', from: "draw_position_winner")
-
-    fill_in "draw_position_score", with: 'Test Score'
-
-    click_button("Update Draw position")
-
-    visit edit_tournament_draw_draw_position_path(tournament_id: 1, draw_id: 1, id: 8)
-
-    expect(page).to have_text('Laird/Watkins')
   end
 
 end
