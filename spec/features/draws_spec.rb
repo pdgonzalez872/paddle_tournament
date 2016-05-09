2469857e266b2c4b02fe84c493ebf4aef52a6dec
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-09 08:33:15 -0500

    Add coverage to tournaments. Done with controllers

--- spec/features/draws_spec.rb
+++ spec/features/draws_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Draws' do
 
-  let(:draw) { Draw.find_by(id: 1) }
+  let(:draw)       { Draw.find_by(id: 1) }
 
   it 'displays the edit draw page correctly' do
     visit edit_tournament_draw_path(tournament_id: draw.tournament.id,
