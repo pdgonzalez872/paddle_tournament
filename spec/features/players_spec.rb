a0cda94aba0ada227231fd30a977587578b34827
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-09 08:00:24 -0500

    Add coverage to players controller

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 describe 'Players feature' do
 
+  let(:player_1) { Player.find_by(name: 'Laird/Watkins') }
+
   it 'notifies the customer that it will be implemented soon' do
     visit players_path
 
     expect(page).to have_text("This feature will be implemented soon!!")
+  end
+
+  it 'displays the players matches' do
+    visit player_path(id: player_1.id)
+
+    expect(page).to have_text(player_1.name)
   end
 end
