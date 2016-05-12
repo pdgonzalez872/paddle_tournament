d4d60f60927beee622143f66bad6b4a5b0624d32
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-12 08:46:00 -0500

    Start on match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,9 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  let(:match) { Match.create! }
+
+  # relies on seeded data
 
   it 'creates the correct class' do
-    expect(match).to be_instance_of described_class
+    expect(Match.new).to be_instance_of described_class
+  end
+
+  #
+  # associations
+  #
+
+
+  context '#winner' do
+  end
+
+  context '#loser' do
+  end
+
+  context '#calculate_loser' do
+  end
+
+  context '#update_winner_and_loser' do
+  end
+
+  context '#update_score' do
+  end
+
+  context '#display_previous_match_score' do
+  end
+
+  context '#display_time' do
+  end
+
+  context '#display_location' do
+  end
+
+  context '#has_two_players?' do
+  end
+
+  context '#has_only_one_player?' do
+  end
+
+  context '#has_adjacent_players?' do
+  end
+
+  context '#no_players_are_nil?' do
+  end
+
+  context '#top_player_is_nil?' do
+  end
+
+  context 'bottom_player_is_nil?' do
+  end
+
+  context '.round' do
   end
 end
