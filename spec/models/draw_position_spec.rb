494e7ecb9c7cd66934ea0d65ad1fbb2725be4a72
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-12 07:37:43 -0500

    Working on models

--- spec/models/draw_position_spec.rb
+++ spec/models/draw_position_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe DrawPosition, type: :model do
-  let(:player_ex)        { Player.new(name: 'Kelsey Laird') }
+  let(:player)           { Player.new(name: 'Kelsey Laird') }
   let(:draw_position)    { DrawPosition.new }
 
   it 'creates the correct class' do
     expect(draw_position).to be_instance_of described_class
   end
 
-  context '#find_previous_match' do
-    let(:current_match) { Match.new(match_number: 4) }
+  context 'Associations' do
+    it { should belong_to(:match) }
+    it { should belong_to(:draw) }
 
-    xit 'finds the previous match relative to draw position' do
+    it { should have_many(:draw_positions_players) }
+    it { should have_many(:players) }
+  end
+
+  context '#has_player' do
+    it 'has a player' do
+      draw_position.players << player
+      expect(draw_position.players.first).to eq player
+      expect(draw_position.has_player?).to eq false
     end
+  end
+
+  context '#previous_match_score' do
+  end
+
+  context '#find_previous_match' do
+  end
+
+  context '#fix_human_error' do
   end
 end
