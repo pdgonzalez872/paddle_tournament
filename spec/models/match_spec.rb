b08bdd37bd1fb3ce67b6f316113b5b5d310cf963
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-14 16:52:13 -0500

    Continue adding tests to match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Match, type: :model do
 
   # relies on seeded data
 
+  let(:draw)        { Draw.find(1) }
+
+  let(:first_match) { draw.matches.find_by(match_number: 16) }
+
+  let(:second_match) { draw.matches.find_by(match_number: 8) }
+
+  let(:player_options) { "[{\"id\":1,\"name\":\"Laird/Watkins\",\"email\":null,\"phone_number\":null,\"created_at\":\"2016-05-14T21:05:15.703Z\",\"updated_at\":\"2016-05-14T21:05:15.703Z\",\"current_draw_position\":null,\"draw_position_id\":null},{\"id\":2,\"name\":\"Gartzke/Schacherer\",\"email\":null,\"phone_number\":null,\"created_at\":\"2016-05-14T21:05:15.722Z\",\"updated_at\":\"2016-05-14T21:05:15.722Z\",\"current_draw_position\":null,\"draw_position_id\":null}]" }
+
+  let(:player_1)    { Player.find_by(name: 'Laird/Watkins') }
+  let(:player_2)    { Player.find_by(name: 'Gartzke/Schacherer') }
+
   it 'creates the correct class' do
     expect(Match.new).to be_instance_of described_class
   end
@@ -11,29 +22,55 @@ RSpec.describe Match, type: :model do
   context 'associations' do
     it { should belong_to(:location) }
     it { should belong_to(:draw) }
-    
+
     it { should have_many(:draw_positions) }
     it { should have_many(:draw_positions_players) }
     it { should have_many(:players) }
   end
 
-  context '#winner' do
+  context 'winners and losers' do
+
+    before do
+      first_match.update_winner_and_loser(winner: player_1, player_options: player_options)
+    end
+
+    context '#winner' do
+      it 'returns the winner of a match' do
+        expect(first_match.winner).to eq player_1
+      end
+    end
+
+    context '#loser' do
+      it 'returns the loser of a match' do
+        expect(first_match.loser).to eq player_2
+      end
+    end
+
+    context '#update_winner_and_loser' do
+      it 'allocates data correctly' do
+        expect(first_match.winner).to eq player_1
+        expect(first_match.loser).to eq player_2
+      end
+    end
+    context '#update_score' do
+      it 'updates the score correctly' do
+        expect(first_match.score).to eq(nil)
+        first_match.update_score('6/1 6/1')
+        expect(first_match.score).to eq('6/1 6/1')
+      end
+    end
+    context '#display_previous_match_score' do
+      it 'displays the previous match score correctly' do
+        p first_match.players.length
+        p second_match.players.length
+        first_match.update_winner_and_loser(winner: player_1, player_options: player_options)
+        p first_match.players.length
+        p second_match.players.length
+        # p first_match.players   
+      end
+    end
   end
 
-  context '#loser' do
-  end
-
-  context '#calculate_loser' do
-  end
-
-  context '#update_winner_and_loser' do
-  end
-
-  context '#update_score' do
-  end
-
-  context '#display_previous_match_score' do
-  end
 
   context '#display_time' do
   end
@@ -56,9 +93,14 @@ RSpec.describe Match, type: :model do
   context '#top_player_is_nil?' do
   end
 
-  context 'bottom_player_is_nil?' do
+  context '#bottom_player_is_nil?' do
+
   end
 
   context '.round' do
+    it 'fetches matches based on round name' do
+      result = Match.round('final')
+      expect(result.length).to eq(5)
+    end
   end
 end
