ee8048e694587767df245da247fff2d12b8cf2fc
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-05-14 17:35:12 -0500

    100 in match.rb

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -5,13 +5,9 @@ RSpec.describe Match, type: :model do
   # relies on seeded data
 
   let(:draw)        { Draw.find(1) }
-
   let(:first_match) { draw.matches.find_by(match_number: 16) }
-
   let(:second_match) { draw.matches.find_by(match_number: 8) }
-
   let(:player_options) { "[{\"id\":1,\"name\":\"Laird/Watkins\",\"email\":null,\"phone_number\":null,\"created_at\":\"2016-05-14T21:05:15.703Z\",\"updated_at\":\"2016-05-14T21:05:15.703Z\",\"current_draw_position\":null,\"draw_position_id\":null},{\"id\":2,\"name\":\"Gartzke/Schacherer\",\"email\":null,\"phone_number\":null,\"created_at\":\"2016-05-14T21:05:15.722Z\",\"updated_at\":\"2016-05-14T21:05:15.722Z\",\"current_draw_position\":null,\"draw_position_id\":null}]" }
-
   let(:player_1)    { Player.find_by(name: 'Laird/Watkins') }
   let(:player_2)    { Player.find_by(name: 'Gartzke/Schacherer') }
 
@@ -59,43 +55,44 @@ RSpec.describe Match, type: :model do
         expect(first_match.score).to eq('6/1 6/1')
       end
     end
-    context '#display_previous_match_score' do
-      it 'displays the previous match score correctly' do
-        p first_match.players.length
-        p second_match.players.length
-        first_match.update_winner_and_loser(winner: player_1, player_options: player_options)
-        p first_match.players.length
-        p second_match.players.length
-        # p first_match.players   
+  end
+
+  context 'state' do
+
+    let(:dummy_match) { Match.new }
+
+    context '#display_time' do
+      it 'displays time in the proper format' do
+        expect(first_match.display_time).to eq('12:00')
+      end
+
+      it 'does not display time if none is given' do
+        expect(dummy_match.display_time).to eq('?')
+      end
+    end
+
+    context '#display_location' do
+      it 'displays location correctly' do
+        expect(first_match.display_location).to eq('GVC')
+      end
+
+      it 'does not display the location if none is given' do
+        expect(dummy_match.display_location).to eq('?')
       end
     end
   end
 
-
-  context '#display_time' do
+  context 'too many dependencies' do
+    # This model has a lot of dependencies and these features are tested here:
+    # spec/features/advance_players_spec.rb
+    context '#has_two_players?'
+    context '#has_only_one_player?'
+    context '#has_adjacent_players?'
+    context '#no_players_are_nil?'
+    context '#top_player_is_nil?'
+    context '#bottom_player_is_nil?'
   end
 
-  context '#display_location' do
-  end
-
-  context '#has_two_players?' do
-  end
-
-  context '#has_only_one_player?' do
-  end
-
-  context '#has_adjacent_players?' do
-  end
-
-  context '#no_players_are_nil?' do
-  end
-
-  context '#top_player_is_nil?' do
-  end
-
-  context '#bottom_player_is_nil?' do
-
-  end
 
   context '.round' do
     it 'fetches matches based on round name' do
